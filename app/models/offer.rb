66a7ad5d8279270d3b29a71ecb7b73f066be077d
Emilio Wuerges <wuerges@gmail.com>
2016-05-25 15:48:59 -0300

    ok

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,8 +5,7 @@ class Offer < ActiveRecord::Base
   def self.create_by_names!(codigo, nome, professor, turno, fase, ativo)
 
     to_bool = {'S' =>  true, 'N' => false }
-
-    puts codigo, nome, professor, turno, fase, ativo
+    from_turno = {'M'=> 1, 'N' => 2}
 
     c = Course.find_or_create_by!(code: codigo, name: nome)
     p = Professor.find_or_create_by!(name: professor)
@@ -14,8 +13,16 @@ class Offer < ActiveRecord::Base
       professor: p, 
       course: c,
       semester: fase.to_i,
-      shift: turno.to_i,
+      shift: from_turno[turno],
       active: to_bool[ativo]
     )
   end
+
+
+  def turno 
+    to_turno = {1 => 'Matutino', 2 => 'Noturno'}
+    to_turno[shift]
+
+  end
+
 end
