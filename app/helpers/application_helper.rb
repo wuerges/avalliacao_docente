5eb2b29edd31e4c781fa311b3c00a872cda1bb61
Emilio Wuerges <wuerges@gmail.com>
2016-09-14 14:05:38 -0300

    Fixed colors in charts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,17 @@ module ApplicationHelper
 
   def teste(as)
     counts = Hash.new(0)
+    ["péssimo", "ruim", "regular", "bom", "excelente"].each do |v| 
+      counts[v] = 0
+    end
+    ["muito fácil", "fácil", "médio", "difícil", "muito difícil"].each do |v|
+      counts[v] = 0
+    end
+
     as.each do |q,v|
       counts[v] += 1
     end
-    pie_chart counts
+
+    pie_chart counts.sort.reverse
   end
 end
